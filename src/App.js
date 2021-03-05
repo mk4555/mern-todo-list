4a002852d9da8f23e092853d95ca03f60134591c
Min Kim <minkimcodes@gmail.com>
2021-03-05 03:49:05 -0500

    axios installed, basic get request on app for list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-function App() {
-  return <div></div>;
-}
-
-export default App;
+import React, { useState, useEffect } from "react";
+import { List } from "./components/List";
+import axios from "axios";
+export const App = () => {
+  const [lists, setLists] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/lists/")
+      .then((res) => setLists(res.data))
+      .catch((err) => console.log(err));
+  });
+  return (
+    <div>
+      {lists.map((list) => (
+        <List data={list} />
+      ))}
+    </div>
+  );
+};
---
+++ src/components/List.js
@@ -0,0 +1,4 @@
+import React from "react";
+export const List = (props) => {
+  return <div>{props.data.title}</div>;
+};
