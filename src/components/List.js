a4031a5ce2a971ec7d76d031ec98315f62e1a032
Min Kim <minkimcodes@gmail.com>
2021-03-10 16:58:46 -0500

    List wrapped in a card

--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,15 @@
 import React from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
 export const List = (props) => {
   return (
-    <div>
-      <h1>{props.data.title}</h1>
-      {props.data.items.map((item) => {
-        return <div key={item._id}>{item.body}</div>;
-      })}
-    </div>
+    <Card variant="outlined">
+      <CardContent>
+        <h1>{props.data.title}</h1>
+        {props.data.items.map((item) => {
+          return <div key={item._id}>{item.body}</div>;
+        })}
+      </CardContent>
+    </Card>
   );
 };
